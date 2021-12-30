137d9a16421dc523b59b79f4649b72ea0828da1f
Julia Zharova <julia.amp.zharova@gmail.com>
2021-12-30 13:42:25 +0200

    Implicit waiter

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -9,6 +9,7 @@ public class FirstClass {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("https://www.google.com/");
         String searchFieldXpath = "//input[@type=\"text\"]";
@@ -29,23 +30,17 @@ public class FirstClass {
         WebElement searchButton = driver.findElement(By.xpath("//button[@class=\"button button_color_green button_size_medium search-form__submit ng-star-inserted\"]"));
         searchButton.click();
         System.out.println("Current URL after click is " + driver.getCurrentUrl());
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
         WebElement chooseProduct = driver.findElement(By.xpath("//img[@src=\"https://content2.rozetka.com.ua/goods/images/big_tile/27924167.jpg\"]"));
         System.out.println("Search product is displayed " + chooseProduct.isDisplayed());
         System.out.println("Product name " + driver.getTitle());
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         chooseProduct.click();
-        //Thread.sleep(3500);
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.navigate().refresh();
 
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         WebElement buyButton = driver.findElement(By.xpath("//div[2]/rz-product-main-info/div[2]/div/ul/li[1]/app-product-buy-btn/app-buy-button/button"));
         System.out.println("Search button is displayed " + buyButton.isDisplayed());
         buyButton.click();
 
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         driver.close();
     }
 }
